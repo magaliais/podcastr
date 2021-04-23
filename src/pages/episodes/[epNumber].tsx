9cbda6034835fe1f22de86da18ed054addc65935
Gabriel Albuquerque Magalhães <gabriel.ashm@hotmail.com>
2021-04-23 13:47:20 -0300

    buttons and slider funcs (shuffle, loop, next, previous, time handle)

--- src/pages/episodes/[epNumber].tsx
+++ src/pages/episodes/[epNumber].tsx
@@ -8,13 +8,14 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 import { api } from '../../services/api';
 
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
+import { usePlayer } from '../../contexts/PlayerContext';
 import convertDurationToTimeString from '../../utils/convertDurationToTimeString';
 
 import styles from './episode.module.scss'; 
@@ -37,16 +38,15 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
-  const router = useRouter();
-  
-  if (router.isFallback) {  // se a página estiver em processo de carregamento 
-    return(
-      <p>Carregando...</p>
-    )
-  }
+  const {play } = usePlayer();
 
   return(
     <div className={styles.episode}>
+      
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
+
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button">
@@ -59,7 +59,7 @@ export default function Episode({ episode }: EpisodeProps) {
           src={episode.thumbnail} 
           objectFit="cover" 
         />
-        <button type="button">
+        <button type="button" onClick={() => play(episode)}>
           <img src="/play.svg" alt="Tocar episódio"/>
         </button>
       </div>
